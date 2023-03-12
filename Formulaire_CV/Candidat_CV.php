c61d713c197859c0e7ff57ff1b82aae15201a4d9
KhaoulaAbdessamad <khaoulaabdessamad2002@gmail.com>
2023-03-12 13:15:19 +0100

    Version 5

--- Formulaire_CV/Candidat_CV.php
+++ Formulaire_CV/Candidat_CV.php
@@ -5,6 +5,8 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="Form.css">
         <link rel="stylesheet" href="navbar.css">
+        <script src="https://kit.fontawesome.com/b356ad9dc8.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../footer.css">
         <script src="Form.js"></script>
     </head>
     <body>
@@ -19,7 +21,7 @@
             <nav class="nav-bar">
                 <ul>
                     <li>
-                        <a href="../index.html">Acceuil</a>
+                        <a href="../index.html">Accueil</a>
 
                     </li>
                     <li>
@@ -40,7 +42,7 @@
                 </ul>
             </nav>
         </header>
-
+        <main>
         <section>
             <h2>Curriculum Vitae</h2>
             <br>
@@ -192,7 +194,40 @@
                 <input type="reset" value="Annuler">
             </form>
         </section>
-       
+        </main>
+        <footer id="Contacts">
+            <div class="main">
+                <!-- Facebook Icon -->
+                <div class="icon fb">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <a href="">Facebook</a>
+                </div>
+        
+                <!-- Twitter Icon -->
+                <div class="icon twt">
+                    <i class="fa-brands fa-twitter"></i>
+                    <a href="">Twitter</a>
+                </div>
+        
+                <!-- Linkedin Icon -->
+                <div class="icon lnk">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                    <a href="">Linkedin</a>
+                </div>
+        
+                <!-- GitHub Icon -->
+                <div class="icon git">
+                    <i class="fa-brands fa-github"></i>
+                    <a href="">GitHub</a>
+                </div>
+        
+                <!-- YouTube Icon -->
+                <div class="icon yt">
+                    <i class="fa-brands fa-youtube"></i>
+                    <a href="">YouTube</a>
+                </div>
+            </div>
+        </footer>
         
         <script>
             btn=document.querySelector(".BarBtn");
